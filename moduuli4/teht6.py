8f46cd1fea27347550af5631e1bab7e953c7d2de
EliasKko <eliaskko@metropolia.fi>
2025-01-23 13:08:59 +0200

    mod6

--- moduuli4/teht6.py
+++ moduuli4/teht6.py
@@ -6,13 +6,8 @@ B = 0
 for i in range(N):
     x = random.randint(-10, 10)
     y = random.randint(-10, 10)
-    list1.append(x)
-    list1.append(y)
     A = A + 1
     if x^2+y^2<10:
         B = B + 1
-        list2.append(x)
-        list2.append(y)
 X = 4 * B / A
-print(A, B)
 print("Piin likiarvo= ", X)
